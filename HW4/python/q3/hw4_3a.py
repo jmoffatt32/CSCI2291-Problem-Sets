270de1cb90f7c9c9c391ec1fc3c69c20e679da13
jmoffatt32 <jmoffatt32@gmail.com>
2022-03-22 08:49:14 -0400

    updated git ignore

--- HW4/python/q3/hw4_3a.py
+++
@@ -1,7 +0,0 @@
-def iso_code_to_int(code):
-    out = 0
-    for i, c in enumerate(code):
-        num = ord(c)
-        power = len(code) - (i + 1)
-        out += num * (256 ** (power))
-    return out
--- HW4/python/q4/hw4_4a.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def clean_string(string):
-    return string.replace("\"", "").strip()
-
-geographic = pd.read_csv("HW4/python/dataset/countries_codes_and_coordinates.csv")
-geographic["Alpha-3 code"] = geographic["Alpha-3 code"].apply(clean_string)
-geographic["Latitude (average)"] = geographic["Latitude (average)"].apply(clean_string)
